b5991a4cc08a83426c557f0d554610ef6efed302
ksy <ksy8230@hanmail.net>
2021-07-17 10:43:44 +0900

    노드에서 process 정보 확인 / setTimeout, nextTick 함수

--- 6-process/app.js
+++ 6-process/app.js
@@ -1,1 +1,18 @@
 const process = require("process");
+
+
+setTimeout(() => {
+    console.log('setTimeout');
+}, 0);
+
+process.nextTick(() => {
+    console.log('nextTick');
+});
+
+for(let i = 0; i <100; i++ ){
+    console.log('loop');
+}
+
+/**
+ * nextTick은 태스크큐에 다른 콜백함수가 들어있어도 순서를 무시하고 제일 우선순위를 높여서 먼저 수행
+ */
