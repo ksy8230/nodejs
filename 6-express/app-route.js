b4f8d05ebae4ee9803531b9dc12df6604e9f598f
ksy <ksy8230@hanmail.net>
2021-08-17 17:20:07 +0900

    express 내부 미들웨어 설정하기 - static

--- 6-express/app-route.js
+++ 6-express/app-route.js
@@ -5,7 +5,17 @@ import userRouter from './router/user.js';
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json()); // REST API 에서 Body를 파싱할 때 사용
+app.use(express.urlencoded({ extended: false })); // HTML FORM 에서 Body를 파싱할 때 사용
+
+const options = {
+    dotfiles: 'ignore',
+    maxAge: '1d', // 캐시 기간
+    setHeaders : function (res, path, stat) {
+        res.set('x-timestamp', Date.now());
+    }
+};
+app.use(express.static('public', options));
 
 app.use('/posts', postRouter);
 app.use('/users', userRouter);
