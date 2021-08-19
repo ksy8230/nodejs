cd97de9531826671a34bb45b8be975c1c878997e
ksy <ksy8230@hanmail.net>
2021-08-19 16:06:49 +0900

    express 외부 미들웨어들

--- 7-cors/app.js
+++ 7-cors/app.js
@@ -1,21 +1,27 @@
 import express from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import helmet from 'helmet';
 
 const app = express();
 
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan('tiny'));
 app.use(
     cors({
         origin: ['http://127.0.0.1:5500'],
+        optionsSuccessStatus: 200,
+        credentials: true,
     }),
 );
-
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, DELETE');
-    next();
-});
+app.use(helmet());
 
 app.get('/', (req, res) => {
+    console.log();
+    console.log(req.body);
+    console.log(req.cookies);
     res.send('Welcome!');
 });
 
