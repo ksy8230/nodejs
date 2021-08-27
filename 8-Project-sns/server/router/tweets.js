9670623835f3f6d4b6846f8679af7a07de120ad8
ksy <ksy8230@hanmail.net>
2021-08-27 10:00:39 +0900

    테스트 프로젝트-sns : 유효성 검사 추가

--- 8-Project-sns/server/router/tweets.js
+++ 8-Project-sns/server/router/tweets.js
@@ -1,18 +1,21 @@
 import express from 'express';
 import 'express-async-errors';
+import {body} from 'express-validator';
 import * as tweetRepository from '../controller/tweets.js';
+import {validate} from '../middleware/validator.js';
 
 const router = express.Router();
 
+const validateTweet = [body('text').trim().isLength({min: 3}).withMessage('텍스트는 3글자 이상 되어야 한다'), validate];
 // GET /tweets
 // GET /tweets?username=:username
 router.get('/', tweetRepository.getTweets);
 // GET /tweets/:id
 router.get('/:id', tweetRepository.getTweet);
 // POST /tweets
-router.post('/', tweetRepository.createTweet);
+router.post('/', validateTweet, tweetRepository.createTweet);
 // PUT /tweets/:id
-router.put('/:id', tweetRepository.updateTweet);
+router.put('/:id', validateTweet, tweetRepository.updateTweet);
 // DELETE /tweets/:id
 router.delete('/:id', tweetRepository.deleteTweet);
 
